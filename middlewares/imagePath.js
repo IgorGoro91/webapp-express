da526a9a7b200c23b009585e7f4a71d0f95a6086
IgorGoro91 <igorysc@gmail.com>
2025-03-17 17:41:15 +0100

    add:import

--- middlewares/imagePath.js
+++ middlewares/imagePath.js
@@ -1,3 +1,5 @@
 function setImagePath (req, res, next){
     req.imagePath = `${req.protocol}://${req.get('host')}/img/movies/`
 }
+
+export default setImagePath
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -3,6 +3,7 @@ import express from 'express'
 const router = express.Router()
 import {index, show, destroy} from '../controllers/moviesController.js'
 
+import ImagePathMiddleware from '../middlewares/imagePath.js'
 
 
 
