88ef9f7f3612c8062b575a4144c27f113df7fc5d
IgorGoro91 <igorysc@gmail.com>
2025-03-18 23:50:41 +0100

    add:correzione

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 import express from "express";
 import moviesRouter from './routes/moviesRouter.js'
 
+
+
 const app = express();
+
+import cors from 'cors'
+
 const port = process.env.SERVER_PORT  || 5000;
+
+app.use(cors({
+    origin: process.env.FRONTEND_APP
+}));
 
 
 app.use(express.static('public'))
