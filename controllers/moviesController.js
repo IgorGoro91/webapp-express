8b5ee7281f2dfcf5fd37c0b48e7891e72d6d4263
IgorGoro91 <igorysc@gmail.com>
2025-03-20 00:36:36 +0100

    add:..

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -88,4 +88,26 @@ function destroy(req, res){
 
 }
 
-export{index, show, destroy, update}
+
+function storRewiew(req, res){
+    const {id} = req.params
+
+    const {text, name, vote} = req.body
+
+    const sql = 'INSERT INTO reviews (text , name, vote, movie_id) VALUES (?,?,?,?)'
+
+    connection.query(sql, [text, name, vote, id],(err, results)=>{
+        if(err) return res.status(500).json({
+            error: 'Database Error StoreReview'
+        })
+
+        res.status(201)
+        res.json({
+            message: 'review Added',
+            id: results.insertId
+        })
+    })
+
+}
+
+export{index, show, destroy, update, storRewiew}
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -1,25 +1,29 @@
 import express from 'express'
 
 const router = express.Router()
-import {index, show, destroy, update} from '../controllers/moviesController.js'
+import {index, show, destroy, update, storRewiew} from '../controllers/moviesController.js'
 
 import ImagePathMiddleware from '../middlewares/imagePath.js'
 
 
-
+//localhost:5000/movies
 router.get( '/', index)
 
 
-
+//localhost:5000/movies/:id
 router.get('/:id', show)
 
 
-
+//localhost:5000/movies/:id
 router.delete('/:id', destroy)
 
 
-
+//localhost:5000/movies/:id
 router.patch('/:id', update)
+
+
+//localhost:5000/movies/:id/reviews
+router.post( '/:id/reviews', storRewiew)
 
 
 export default router
